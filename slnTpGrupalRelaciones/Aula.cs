8895f50ad1e7e9ee894fb10360e70e76280b4972
Hlorenzoo <hernanlorenzo97@hotmail.com>
2023-01-13 15:46:14 -0300

    clases

--- slnTpGrupalRelaciones/Aula.cs
+++ slnTpGrupalRelaciones/Aula.cs
@@ -8,5 +8,15 @@ namespace slnTpGrupalRelaciones
 {
     public class Aula
     {
+        public Aula() { }
+        public Aula(int capacidad, string codigo)
+        {
+           Capacidad= capacidad;
+           Codigo= codigo;  
+        }
+        public int Capacidad { get; set; }
+        public string Codigo { get; set; }
+        public Clase Clase { get; set; }
+
     }
 }
--- slnTpGrupalRelaciones/Clase.cs
+++ slnTpGrupalRelaciones/Clase.cs
@@ -8,5 +8,15 @@ namespace slnTpGrupalRelaciones
 {
     public class Clase
     {
+        public Clase() { }
+        public Clase(DateTime fin, DateTime inicio) 
+        {
+            Fin= fin;
+            Inicio= inicio;
+        }
+        public DateTime Fin { get; set; }
+        public DateTime Inicio { get; set; }
+        public Aula Aula { get; set; }
+
     }
 }
